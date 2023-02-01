1d552409ebc7e66a528266f452c60bf77b25c581
Jeff Sahim <jeffreysahim@gmail.com>
2023-02-01 16:22:58 -0500

    Build Game class skeleton

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,24 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
-  constructor() {}
-
+  constructor(currentRound) {
+    this.currentRound = currentRound
+  }
+  start(prototypeQuestions){
+    const cardsArray = []
+    prototypeQuestions.forEach(element => {
+      cardsArray.push(new Card(element.id, element.question, element.answers, element.correctAnswer))
+    })
+    const newDeck = new Deck(cardsArray);
+    const newRound = new Round(newDeck);
+    printMessage(newDeck, newRound)
+    printQuestion(newRound)
+  }
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
--- test/Game-test.js
+++ test/Game-test.js
@@ -2,3 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Game = require('../src/Game');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+const Round = require('../src/Round');
