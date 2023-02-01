efad3f2f9e42bfc79dd6f37aa1a2558b8e39a330
Jeff Sahim <jeffreysahim@gmail.com>
2023-02-01 17:43:38 -0500

    Finish Game class

--- src/Game.js
+++ src/Game.js
@@ -1,23 +1,22 @@
-const data = require('./data');
-const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
 class Game {
-  constructor(currentRound) {
-    this.currentRound = currentRound
+  constructor() {
+    this.currentRound;
   }
-  start(prototypeQuestions){
+  start(questions){
     const cardsArray = []
-    prototypeQuestions.forEach(element => {
+    questions.forEach(element => {
       cardsArray.push(new Card(element.id, element.question, element.answers, element.correctAnswer))
     })
     const newDeck = new Deck(cardsArray);
-    const newRound = new Round(newDeck);
-    printMessage(newDeck, newRound)
-    printQuestion(newRound)
+    console.log(cardsArray)
+    this.currentRound = new Round(newDeck);
+    this.printMessage(newDeck, this.currentRound)
+    this.printQuestion(this.currentRound)
   }
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
--- test/Game-test.js
+++ test/Game-test.js
@@ -1,8 +1,41 @@
 const chai = require('chai');
 const expect = chai.expect;
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
 
 const Game = require('../src/Game');
-const Card = require('../src/Card');
-const Deck = require('../src/Deck');
-const Turn = require('../src/Turn');
-const Round = require('../src/Round');
+// const Card = require('../src/Card');
+// const Deck = require('../src/Deck');
+// const Turn = require('../src/Turn');
+// const Round = require('../src/Round');
+
+
+describe('Game', function() {
+  let game;
+
+  beforeEach('game instance', () => {
+    game = new Game()
+  });
+
+  it('should be a function', () => {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Game', () => {
+    expect(game).to.be.an.instanceof(Game);
+    game.start(prototypeQuestions)
+  }); 
+
+  it.skip('should be able to start the game', () => {
+
+    expect(game.currentRound).to.be.an('Object');
+  });  
+
+  it.skip('should be a able to count the number of cards in the deck', () => {
+    const countStack = deck.countCards()
+
+    expect(countStack).to.be.a('number');
+    expect(countStack).to.equal(3);
+  });  
+
+});
